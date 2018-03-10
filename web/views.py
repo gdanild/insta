6ea3825870beb07fb59485ca36e7d27fb72cea7d
Danila Grischenko <danila173@gmail.com>
2018-03-11 02:06:23 +0300

    only login

--- web/views.py
+++ web/views.py
@@ -5,44 +5,71 @@ from .InstagramAPI import InstagramAPI
 
 api = ""
 
+
 def InList(a):
     res = []
     for i in a:
         res.append(i["username"])
     return res
 
+def GetPk(username,api):
+    api.searchUsername(username)
+    result = api.LastJson
+    if not result['status'] == 'fail':
+        return result["user"]["pk"]
+    else:
+        raise Exception("Error username")
+
+def GenerateBadUsers(a,b):
+    res = []
+    for i in b:
+        if a.count(i) == 0:
+            res.append(i)
+    return res
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     form = AddPostForm(csrf_enabled=False)
-    text = "Сервис, который ищет не взаимные подписки :)"
-    return render_template('main.html', form=form, lol=1,text = text)
+    return render_template('main.html', form=form, error_login=False)
+
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
     global api
     form = AddPostForm(csrf_enabled=False)
     if form.validate_on_submit():
-        data = [form.author.data,form.message.data]
-        api = InstagramAPI(data[0], data[1])
-        if (api.login()):
-            a = InList(api.getTotalSelfFollowers()) #Подписчики
-            b = InList(api.getTotalSelfFollowings()) #подписки
-            res = []
-            for i in b:
-                if a.count(i) == 0:
-                    res.append(i)
-            #print("Подписчики: {}\nПодписки: {}".format(a,b))
-            print("Login succes!")
-            return render_template('result.html',users=res)
+        data = [form.author.data, form.message.data]
+        if len(data[1]) != 0:
+            api = InstagramAPI(data[0], data[1])
+            if (not api.login()):
+                print("Can't login!")
+                return render_template('main.html', form=form, error_login=True)
+            else:
+                a = InList(api.getTotalSelfFollowers())  # Подписчики
+                b = InList(api.getTotalSelfFollowings())  # подписки
+                status_unfollow_funct = True
+                # print("Подписчики: {}\nПодписки: {}".format(a,b))
         else:
-            print("Can't login!")
-            return render_template('main.html', form=form, lol=0)
-    return redirect("/")
+            api = InstagramAPI("stolovka.1747", "ToYwjMHa698")
+            api.login()
+            a = InList(api.getTotalFollowers(GetPk(data[0],api)))
+            b = InList(api.getTotalFollowings(GetPk(data[0],api)))
+            #print("Подписчики: {}\nПодписки: {}".format(len(a), len(b)))
+            status_unfollow_funct = False
+
+        return render_template('result.html', users=GenerateBadUsers(a, b), status = status_unfollow_funct)
+
+
+
+    else:
+        return redirect("/")
+
 
 @app.route('/about_us', methods=['GET', 'POST'])
 def about_us():
     return render_template('about_us.html')
+
 
 @app.route('/unfollow/<id_user>', methods=['POST'])
 def unfollow(id_user):
